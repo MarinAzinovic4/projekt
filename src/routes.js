d3bbb14912db731d9de0b4de86c7878e0eb1797b
MarinAzinovic4 <74631574+MarinAzinovic4@users.noreply.github.com>
2021-01-19 23:33:26 +0100

    Add files via upload

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import home from './components/home.vue'
+import login from './components/login.vue'
+import register from './components/register.vue'
+import onama from './components/onama.vue'
+import kontakt from './components/kontakt.vue'
+
+export default [
+    {path:'/',component: home},
+    {path:'/login',component: login},
+    {path:'/register',component: register},
+    {path:'/onama',component: onama},
+    {path:'/kontakt',component: kontakt},
+]
